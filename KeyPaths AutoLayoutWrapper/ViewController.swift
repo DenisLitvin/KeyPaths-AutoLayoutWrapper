112cdb906eae903268a36752c552eec823d26409
Litvin <dl13653@c02x11rthtd6.namdmz.dmzroot.net>
2018-11-01 14:00:07 -0500

    ADd impl

--- KeyPaths AutoLayoutWrapper/ViewController.swift
+++ KeyPaths AutoLayoutWrapper/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let view = UIView()
+        
+        view.addSubview(view, constraints:[
+            equal(\.centerYAnchor, \.bottomAnchor),
+            equal(\.leftAnchor, constant: 10), equal(\.rightAnchor, constant: -10),
+            equal(\.heightAnchor, constant: 100)
+            ])
     }
 
     override func didReceiveMemoryWarning() {
---
+++ KeyPaths AutoLayoutWrapper/Wrapper.swift
@@ -0,0 +1,38 @@
+//
+//  Wrapper.swift
+//  KeyPaths AutoLayoutWrapper
+//
+//  Created by Litvin, Denis [GCB-OT] on 11/1/18.
+//  Copyright © 2018 Litvin, Denis [GCB-OT]. All rights reserved.
+//
+
+import UIKit
+
+typealias Constraint = (UIView, UIView) -> NSLayoutConstraint
+
+extension UIView {
+    func addSubview(_ other: UIView, constraints: [Constraint]) {
+        other.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(other)
+        addConstraints(constraints.map { $0(other, self) })
+    }
+}
+
+func equal<L, Axis>(_ to: KeyPath<UIView, L>,
+                    constant: CGFloat = 0) -> Constraint where L: NSLayoutAnchor<Axis> {
+    return equal(to, to, constant: constant)
+}
+
+func equal<L, Axis>(_ from: KeyPath<UIView, L>,
+                    _ to: KeyPath<UIView, L>,
+                    constant: CGFloat = 0) -> Constraint where L: NSLayoutAnchor<Axis> {
+    return { view1, view2 in
+        view1[keyPath: from].constraint(equalTo: view2[keyPath: to], constant: constant)
+    }
+}
+func equal<L>(_ keyPath: KeyPath<UIView, L>, constant: CGFloat) -> Constraint where L: NSLayoutDimension {
+    return { view1, _ in
+        view1[keyPath: keyPath].constraint(equalToConstant: constant)
+    }
+}
+
