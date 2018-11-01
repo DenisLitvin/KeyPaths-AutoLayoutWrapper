a5aa95bdbbc715ebfe8cc394649a3650ac4e202e
Denis Litvin <dl13653@C02X11RTHTD6>
2018-11-01 13:54:22 -0500

    Initial Commit

---
+++ KeyPaths AutoLayoutWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyPaths AutoLayoutWrapper
+//
+//  Created by Litvin, Denis [GCB-OT] on 11/1/18.
+//  Copyright © 2018 Litvin, Denis [GCB-OT]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
